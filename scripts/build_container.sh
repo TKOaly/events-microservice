8d2d1fe21ab90d892f2c90f70818576237ab7c86
Hugo Holmqvist <hugo.holm@live.com>
2020-02-26 19:51:18 +0200

    up deps

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Building events-microservice container"
-docker build . -t eventbird-tg
+docker build . -t events-microservice
--- scripts/deploy_container.sh
+++ scripts/deploy_container.sh
@@ -3,5 +3,5 @@ set -ex
 echo "Pushing container image to registry.tko-aly.fi"
 docker login registry.tko-aly.fi --username $DEPLOY_USERNAME --password $DEPLOY_PASSWORD
 docker build . -t events-microservice
-docker tag eventbird-tg:latest registry.tko-aly.fi/events-microservice:latest
+docker tag events-microservice:latest registry.tko-aly.fi/events-microservice:latest
 docker push registry.tko-aly.fi/events-microservice:latest
