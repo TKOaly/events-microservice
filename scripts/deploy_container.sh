38d66438f9e9e5c9b40c22ed5ce87017de5415c9
Hugo Holmqvist <hugo.holm@live.com>
2020-04-25 00:58:31 +0300

    Add 10min ttimout

--- scripts/deploy_container.sh
+++ scripts/deploy_container.sh
@@ -11,4 +11,4 @@ docker push $AWS_ECR_URL:latest
 # Trigger ECS update
 curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | tee ./ecs-deploy
 chmod +x ./ecs-deploy
-./ecs-deploy -r eu-west-1 -c christina-regina -n event-service -i $AWS_ECR_URL:latest
+./ecs-deploy -t 600 -r eu-west-1 -c christina-regina -n event-service -i $AWS_ECR_URL:latest
