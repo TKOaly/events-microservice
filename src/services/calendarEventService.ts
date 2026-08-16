44cc938bd4f5e99f7f63d98b4e65d7df5a21f958
Juuso Kosonen <kosonenjusu@gmail.com>
2026-08-17 00:12:56 +0300

    added general quary helppers

--- src/services/calendarEventService.ts
+++ src/services/calendarEventService.ts
@@ -309,7 +309,106 @@ export async function getRegistrationsForCalendarEventId(
   })
 }
 
-function parseQueryResult(row: any): CalendarEvent {
+function addTableNameToColumn(tableName: string, column: string): string {
+  const [realColumn, aliasPart] = column.split(/\s+as\s+/i).map(s => s.trim())
+  return aliasPart
+    ? `${tableName}.${realColumn} as ${aliasPart}`
+    : `${tableName}.${realColumn}`
+}
+
+function addTranslations(
+  query: Knex.QueryBuilder,
+  tableName: string,
+  locale: string,
+  columns: string[],
+) {
+  const translationTable = `${tableName}_translation`
+  const baseFkColumn = `${tableName.replace(/s$/, '')}_id`
+  const knex = query.client
+
+  const selectColumns = [
+    `${tableName}.${baseFkColumn}`,
+    `${translationTable}.locale`,
+    ...columns.map(column => addTableNameToColumn(translationTable, column)),
+  ]
+
+  const translationSubquery =
+    locale === 'fi'
+      ? knex
+          .queryBuilder()
+          .select(selectColumns)
+          .from(translationTable)
+          .where('locale', 'fi')
+      : knex
+          .queryBuilder()
+          .select(selectColumns)
+          .from(translationTable)
+          .whereIn('locale', [locale, 'fi'])
+          .distinctOn(baseFkColumn)
+          .orderBy([
+            baseFkColumn,
+            knex.raw('(locale = ?) desc', [locale]),
+            'locale desc',
+          ])
+
+  query.leftJoin(translationSubquery.as(translationTable), function () {
+    this.on(`${translationTable}.${baseFkColumn}`, '=', `${tableName}.id`)
+  })
+
+  return query
+}
+
+function addEventCategory(
+  query: Knex.QueryBuilder,
+  tableName: string,
+  locale: string,
+  columnsToSelect: { category?: boolean; implicit_alcohol_meter?: boolean },
+) {
+  const knex = query.client
+
+  query.leftJoin(tableName, function () {
+    this.on(`${tableName}.type_id`, '=', 'event_types.id').andOn(
+      knex.raw('event_types.deleted = false'),
+    )
+  })
+
+  if (columnsToSelect.category) {
+    addTranslations(query, locale, 'event_types', ['name as category'])
+  }
+
+  if (columnsToSelect.implicit_alcohol_meter) {
+    query.select('event_types.implicit_alcohol_meter')
+  }
+
+  return query
+}
+
+function addLocation(
+  query: Knex.QueryBuilder,
+  tableName: string,
+  locale: string,
+  columnsToSelect: { location?: boolean; map_link?: boolean },
+) {
+  const knex = query.client
+
+  query.leftJoin(tableName, function () {
+    this.on(`${tableName}.location_id`, '=', 'locations.id').andOn(
+      knex.raw('locations.deleted = false'),
+    )
+  })
+
+  if (columnsToSelect.location) {
+    addTranslations(query, locale, 'locations', ['name as location'])
+  }
+
+  if (columnsToSelect.map_link) {
+    query.select('event_type.map_link')
+  }
+
+  return query
+}
+
+function parseQueryResult(row: any): Event {
   const picked = pick(row, [
     'id',
     'name',
