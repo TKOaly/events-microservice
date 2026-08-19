e2e202f7e113c5ebfe0e3fdf8e24b00a7805879d
Juuso Kosonen <kosonenjusu@gmail.com>
2026-08-20 01:56:59 +0300

    added registration fetching

--- src/database/registrations.ts
+++ src/database/registrations.ts
@@ -1,61 +1,124 @@
-import { db } from './db';
-import { DbAnswer, Registration } from './types';
+import { Knex } from 'knex'
+import { db } from './db'
+import { CustomFieldAnswer, Registration, RegistrationRow } from './types'
+import { addTranslations } from './utils'
 
+export async function getUsersRegistrationForEvent(
+  user_id: number,
+  event_id: number,
+  locale: string,
+): Promise<Registration> {
+  const query = db('registrations')
+  const queryWithRegistrationData = selectRegistrationRows(query)
+
+  queryWithRegistrationData
+    .where('registrations.event_id', event_id)
+    .where('registrations.user_id', user_id)
+
+  const row = await queryWithRegistrationData.first<RegistrationRow>()
+
+  const customFields = await getCustomFieldAnswer(row.registration_id, locale)
+
+  const avecCustomFields = row.avec_registration_id
+    ? await getCustomFieldAnswer(row.avec_registration_id, locale)
+    : undefined
+
+  return formatRegistration(row, customFields, avecCustomFields)
+}
 
 export async function getRegistrationsForCalendarEventId(
-  eventId: number
-): Promise<Array<Registration>> {
-  const registrations = await db
-    .select('registrations.*', 'users.id as user_id')
-    .from('registrations')
-    .leftJoin('users', 'users.id', '=', 'registrations.user_id')
-    .where('registrations.calendar_event_id', eventId);
+  eventId: number,
+  locale: string,
+): Promise<Registration[]> {
+  const query = db('registrations')
+  const queryWithRegistrationData = selectRegistrationRows(query)
 
-  const answers = await db
-    .select(
-      'custom_field_answers.value',
-      'custom_field_answers.registration_id',
-      'custom_fields.name',
-      'custom_fields.id as custom_field_id'
-    )
-    .from('custom_field_answers')
-    .join(
-      'custom_fields',
-      'custom_fields.id',
-      '=',
-      'custom_field_answers.custom_field_id'
-    )
-    .where(
-      'custom_field_answers.registration_id',
-      'IN',
-      registrations.map(r => r.id)
-    );
+  queryWithRegistrationData.where('registrations.calendar_event_id', eventId)
 
-  const answersByRegistrationId = new Map();
+  const rows = await queryWithRegistrationData
 
-  answers.forEach(answer => {
-    if (!answersByRegistrationId.has(answer.registration_id)) {
-      answersByRegistrationId.set(answer.registration_id, [answer]);
-    } else {
-      answersByRegistrationId.get(answer.registration_id).push(answer);
-    }
-  });
+  return Promise.all(
+    rows.map(async row => {
+      const customFields = await getCustomFieldAnswer(row.id, locale)
 
-  return registrations.map(registration => {
-    const answers = answersByRegistrationId.get(registration.id) ?? [];
+      const avecCustomFields = row.avec_registration_id
+        ? await getCustomFieldAnswer(row.avec_registration_id, locale)
+        : undefined
 
-    return {
-      id: registration.id,
-      user_id: registration.user_id,
-      created: registration.created,
-      name: registration.name,
-      email: registration.email,
-      phone: registration.phone,
-      answers: answers.map(formatRegistrationAnswer),
-    };
-  });
+      return formatRegistration(row, customFields, avecCustomFields)
+    }),
+  )
 }
-export function formatRegistrationAnswer(row: DbAnswer) {
+
+function selectRegistrationRows(query: Knex.QueryBuilder) {
+  query.select(
+    'registrations.id',
+    'registrations.user_id',
+    'registrations.created',
+    'registrations.name',
+    'registrations.email',
+    'registrations.phone',
+  )
+
+  query.leftJoin('registrations as avec_registrations', function () {
+    this.on(
+      'avec_registrations.id',
+      '=',
+      'registrations.avec_registration_id',
+    ).andOnNotNull('registrations.avec_registration_id')
+  })
+
+  query.select(
+    'avec_registrations.registration_id as avec_registration_id',
+    'avec_registrations.name as avec_name',
+    'avec_registrations.email as avec_email',
+    'avec_registrations.phone as avec_phone',
+  )
+
+  return query
+}
+
+async function getCustomFieldAnswer(
+  registration_id: number,
+  locale: string,
+): Promise<CustomFieldAnswer[]> {
+  const query = db('custom_fields_answers')
+
+  query.select(
+    'custom_fields_answers.custom_field_id',
+    'custom_fields_answers.value',
+  )
+
+  addTranslations(query, 'custom_fields', locale, ['name'])
+
+  query.where('custom_fields_answers.registration_id', registration_id)
+
+  return query.then(rows => rows.map(formatRegistrationAnswer))
+}
+
+function formatRegistration(
+  row: RegistrationRow,
+  customFieldAnswers: CustomFieldAnswer[],
+  avecCustomFields?: CustomFieldAnswer[],
+): Registration {
+  return {
+    user_id: row.user_id,
+    name: row.name,
+    email: row.email,
+    phone: row.phone,
+    customFieldAnswers: customFieldAnswers,
+    avecRegistration: row.avec_registration_id
+      ? {
+          name: row.avec_name,
+          email: row.avec_email,
+          phone: row.avec_phone,
+          customFieldAnswers: avecCustomFields,
+        }
+      : undefined,
+  }
+}
+
+export function formatRegistrationAnswer(row: any): CustomFieldAnswer {
   return {
     question_id: row.custom_field_id,
     question: row.name,
