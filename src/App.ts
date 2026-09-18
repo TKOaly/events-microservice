8f93c89c2dd287171e230cb783090b2cb71e2069
Juuso Kosonen <kosonenjusu@gmail.com>
2026-09-18 17:10:31 +0300

    fixed router order

--- src/App.ts
+++ src/App.ts
@@ -14,40 +14,30 @@ async function startServer(servicePort: number) {
   // Ping route
   app.get('/ping', (_, res) => res.send('Hello there'))
 
-  const authorizeRequest: express.Handler = (
-    req,
-    res,
-    next
-  ) => {
+  const authorizeRequest: express.Handler = (req, res, next) => {
     if (req.get('X-Token') === process.env.SERVICE_AUTH_TOKEN) {
       return next()
     }
     return res.status(401).json({ error: 'unauthorized' })
   }
 
-  app.get(
-    '/api/events',
-    async (req, res) => {
+  app.get('/api/events', async (req, res) => {
     const fromDate = req.query.fromDate
     try {
       const calendarEvents = await calendarEventService.getAllCalendarEvents(
-          fromDate?.toString()
+        fromDate?.toString(),
       )
       return res.status(200).json(calendarEvents)
     } catch (e) {
       console.log(e)
       res.status(500).json({ error: 'internal server error' })
     }
-    }
-  )
+  })
 
-  app.get(
-    '/api/users/:id/events',
-    authorizeRequest,
-    async (req, res) => {
+  app.get('/api/users/:id/events', authorizeRequest, async (req, res) => {
     try {
       const calendarEvents = await calendarEventService.getEventsForUserId(
-          Number(req.params.id)
+        Number(req.params.id),
       )
       return res.json(calendarEvents)
     } catch (e) {
@@ -62,6 +52,20 @@ async function startServer(servicePort: number) {
       return res.json(templates)
     } catch (e) {
       console.error(e)
+      res.status(500).json({ error: 'internal server error' })
+    }
+  })
+
+  app.get('/api/events/list', async (req, res) => {
+    const fromDate = req.query.fromDate
+    try {
+      const calendarEvents =
+        await calendarEventService.getAllCalendarEventsForEventList(
+          fromDate?.toString(),
+        )
+      return res.status(200).json(calendarEvents)
+    } catch (e) {
+      console.log(e)
       res.status(500).json({ error: 'internal server error' })
     }
   })
@@ -84,7 +88,10 @@ async function startServer(servicePort: number) {
     authorizeRequest,
     async (req, res) => {
       try {
-        const registrations = await calendarEventService.getRegistrationsForCalendarEventId(Number(req.params.id))
+        const registrations =
+          await calendarEventService.getRegistrationsForCalendarEventId(
+            Number(req.params.id),
+          )
 
         return res.json(registrations)
       } catch (e) {
@@ -94,87 +101,70 @@ async function startServer(servicePort: number) {
     },
   )
 
-  app.get(
-    '/api/events/list',
-    async (req, res) => {
-    const fromDate = req.query.fromDate
+  app.get('/api/events/:id/fields', authorizeRequest, async (req, res) => {
     try {
-      const calendarEvents = await calendarEventService.getAllCalendarEventsForEventList(
-          fromDate?.toString()
-      )
-      return res.status(200).json(calendarEvents)
-    } catch (e) {
-      console.log(e)
-      res.status(500).json({ error: 'internal server error' })
-    }
-    }
-  )
-
-  app.get(
-    '/api/events/:id/fields',
-    authorizeRequest,
-    async (req, res) => {
-    try {
-        const fields = await calendarEventService.getCustomFieldsForCalendarEventId(Number(req.params.id))
+      const fields =
+        await calendarEventService.getCustomFieldsForCalendarEventId(
+          Number(req.params.id),
+        )
 
       return res.json(fields)
     } catch (e) {
       console.error(e)
       res.status(500).json({ error: 'internal server error' })
     }
-    },
-  )
+  })
 
   app.post(
     '/api/events',
     express.json(),
     authorizeRequest,
     async (req, res) => {
-        const event = calendarEventService.EventSchema.safeParse(req.body);
+      const event = calendarEventService.EventSchema.safeParse(req.body)
       if (!event.success) {
-          const err = z.prettifyError(event.error);
-          return res.status(400).send(err);
+        const err = z.prettifyError(event.error)
+        return res.status(400).send(err)
       } else {
         try {
           const result = await calendarEventService.addNewEvent(event.data)
           return res.status(201).json({ id: result })
         } catch (e) {
-            console.error(e);
-            res.status(500).json({ error: 'internal server error' });
-          };
+          console.error(e)
+          res.status(500).json({ error: 'internal server error' })
         }
       }
-  );
+    },
+  )
 
   app.put(
     '/api/events/:id',
     express.json(),
     authorizeRequest,
     async (req, res) => {
-      const event = calendarEventService.EventSchema.safeParse(req.body);
+      const event = calendarEventService.EventSchema.safeParse(req.body)
       if (!event.success) {
-        const err = z.prettifyError(event.error);
-        return res.status(400).send(err);
+        const err = z.prettifyError(event.error)
+        return res.status(400).send(err)
       } else {
         try {
           const id = parseInt(req.params.id)
           if (Number.isNaN(id)) {
-            return res.status(404).json({ error: "Not Found" })
-          } else if (!await calendarEventService.isExistingEvent(id)) {
-            return res.status(404).json({ error: "ID Not Found" })
+            return res.status(404).json({ error: 'Not Found' })
+          } else if (!(await calendarEventService.isExistingEvent(id))) {
+            return res.status(404).json({ error: 'ID Not Found' })
           }
           const result = await calendarEventService.updateEvent(id, event.data)
           return res.status(200).json({ id: result })
         } catch (e) {
           console.error(e)
-          return res.status(500).json({ error: "internal server error" })
+          return res.status(500).json({ error: 'internal server error' })
         }
       }
-    }
+    },
   )
 
   app.listen(servicePort, () =>
-    console.log('App listining on port', servicePort)
+    console.log('App listining on port', servicePort),
   )
 }
 
